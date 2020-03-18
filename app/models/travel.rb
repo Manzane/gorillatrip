7be8b991c769c831695c221e1804442d0470d454
marie <marie.blanchet.pro@gmail.com>
2020-03-18 21:06:44 +0100

    add dependent destroy travel and travel_countries

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,6 +1,6 @@
 class Travel < ApplicationRecord
   belongs_to :user
-  has_many :travel_countries
+  has_many :travel_countries, dependent: :destroy
   has_many :countries, through: :travel_countries
 
   validates :travel_start_date, presence: true
