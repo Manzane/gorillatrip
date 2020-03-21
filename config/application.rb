04992bd921df60cfc1b1e849d3ece8d2b875d937
samuelboulery <samuelboulery@gmail.com>
2020-03-21 14:42:42 +0100

    fix problem with import font

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Gorillatrip
           generate.test_framework  :test_unit, fixture: false
         end
     # Initialize configuration defaults for originally generated Rails version.
+    # config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    # config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
