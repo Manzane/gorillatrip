eda3c3c1a8cc65d7f52144931eb57c0238d329a1
marie <marie.blanchet.pro@gmail.com>
2020-03-22 18:06:40 +0100

    Travel Updater fixed

--- app/services/travel_updater.rb
+++ app/services/travel_updater.rb
@@ -10,7 +10,7 @@ class TravelUpdater
     @travel.update(name: @params[:travel][:name], travel_start_date: @params[:travel][:travel_start_date], travel_end_date: @params[:travel][:travel_end_date])
     # fonctionne juste avec un seul pays [0][:id] ci dessous...
     @travel_country = TravelCountry.find(@travel.travel_countries[0][:id])
-    TravelCountry.update(duration: @params[:travel][:travel_countries_attributes]["0"][:duration], country_id: @params[:travel][:country_ids].to_i)
+    @travel_country.update(duration: @params[:travel][:travel_countries_attributes]["0"][:duration], country_id: @params[:travel][:country_ids].to_i)
     # @params[:travel][:country].each do |c|
     #   TravelCountry.create(duration: @params[:travel][:duration], travel: @travel, country: @params[:travel][:country])
     # end
