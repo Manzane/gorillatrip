59d17b0483c0fcc69dd48d877e69e778ef8a5aec
marie <marie.blanchet.pro@gmail.com>
2020-03-22 06:39:48 +0100

    Visas Controller - Index nd Show actions

--- app/controllers/visas_controller.rb
+++ app/controllers/visas_controller.rb
@@ -1,2 +1,22 @@
 class VisasController < ApplicationController
+    before_action :set_visa, only: :show
+    before_action :set_country
+
+  def index
+    @visas = Visa.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_visa
+    @visa = Visa.find(params[:id])
+
+  end
+
+  def set_country
+    @country = Country.find(params[:country_id])
+  end
 end
