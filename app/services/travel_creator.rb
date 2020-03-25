96cd38dcffce77c5a59225e92a08d06d718274f9
marie <marie.blanchet.pro@gmail.com>
2020-03-25 12:19:14 +0100

    strong params implemented

--- app/services/travel_creator.rb
+++ app/services/travel_creator.rb
@@ -5,8 +5,8 @@ class TravelCreator
   end
   attr_reader :travel
   def save
-    @travel = Travel.create(name: @params[:travel][:name], travel_start_date: @params[:travel_start_date], travel_end_date: @params[:travel_end_date], user: @user )
-    TravelCountry.create(duration: @params[:travel][:travel_countries], travel: @travel, country_id: @params[:travel][:country_ids].to_i)
+    @travel = Travel.create(name: @params[:name], travel_start_date: @params[:travel_start_date], travel_end_date: @params[:travel_end_date], user: @user )
+    TravelCountry.create(duration: @params[:travel_countries_attributes]["0"][:duration].to_i, travel: @travel, country_id: @params[:countries_attributes]["0"][:id].to_i)
     # @params[:travel][:country].each do |c|
     #   TravelCountry.create(duration: @params[:travel][:duration], travel: @travel, country: @params[:travel][:country])
     # end
--- app/services/travel_updater.rb
+++ app/services/travel_updater.rb
@@ -1,16 +1,17 @@
 class TravelUpdater
-  def initialize(params, user)
+  def initialize(set_travel, params, user)
+    @travel = set_travel
     @params = params
     @user = user
   end
   attr_reader :travel
   attr_reader :travel_country
   def save
-    @travel = Travel.find(@params[:id])
-    @travel.update(name: @params[:travel][:name], travel_start_date: @params[:travel_start_date], travel_end_date: @params[:travel_end_date])
+    # @travel = Travel.find(@params[:id])
+    @travel.update(name: @params[:name], travel_start_date: @params[:travel_start_date], travel_end_date: @params[:travel_end_date])
     # fonctionne juste avec un seul pays [0][:id] ci dessous...
-    @travel_country = TravelCountry.find(@travel.travel_countries[0][:id])
-    @travel_country.update(duration: @params[:travel][:travel_countries_attributes]["0"][:duration], country_id: @params[:travel][:country_ids].to_i)
+    @travel_country = TravelCountry.find(@params[:travel_countries_attributes]["0"][:id].to_i)
+    @travel_country.update1(duration: @params[:travel_countries_attributes]["0"][:duration].to_i, country_id: @params[:countries_attributes]["0"][:id].to_i)
     # @params[:travel][:country].each do |c|
     #   TravelCountry.create(duration: @params[:travel][:duration], travel: @travel, country: @params[:travel][:country])
     # end
