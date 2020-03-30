6ed96a7ebfaa15c1daacc6f70af9eda79c79cebc
marie <marie.blanchet.pro@gmail.com>
2020-03-30 14:01:52 +0200

    add confidentiality page (route - add in pages controller + view)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :maintenance]
+  skip_before_action :authenticate_user!, only: [:home, :maintenance, :confidentiality]
 
   def home
   end
 
   def maintenance
+  end
+
+  def confidentiality
   end
 end
