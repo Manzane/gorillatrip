b686a304750459b0afc2df3ed7388fdf0b79ecc4
marie <marie.blanchet.pro@gmail.com>
2020-04-10 19:05:08 +0200

    creation of the Visa Progression table with visa_id and travel_country_id references and the boolean done

--- app/models/travel_country.rb
+++ app/models/travel_country.rb
@@ -1,6 +1,7 @@
 class TravelCountry < ApplicationRecord
   belongs_to :travel
   belongs_to :country
+  has_many :visa_progressions
 
   validates :start_date, presence: true
   validates :end_date, presence: true
--- app/models/visa.rb
+++ app/models/visa.rb
@@ -1,3 +1,5 @@
 class Visa < ApplicationRecord
   belongs_to :country
+  has_many :visa_progressions
+
 end
