16dd5b8b1884bf0b60315a22d92d9985ef166ce9
marie <marie.blanchet.pro@gmail.com>
2020-04-10 13:26:41 +0200

    User can edit information without current password confirmation, cannot change email address or password though

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
 
 
   scope "(:locale)", locale: /en|es/ do
