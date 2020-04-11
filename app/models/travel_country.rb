e3a3bbeedc151f5c5c3831203b786c8a1a10d139
marie <marie.blanchet.pro@gmail.com>
2020-04-11 12:45:36 +0200

    preparation for progression vaccines

--- app/models/travel_country.rb
+++ app/models/travel_country.rb
@@ -1,7 +1,7 @@
 class TravelCountry < ApplicationRecord
   belongs_to :travel
   belongs_to :country
-  has_many :visa_progressions
+  has_many :visa_progressions, dependent: :destroy
 
   validates :start_date, presence: true
   validates :end_date, presence: true
