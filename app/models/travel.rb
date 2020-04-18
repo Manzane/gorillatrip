ac102245f2ba098427c3a61a04a3e943e701caa0
marie <marie.blanchet.pro@gmail.com>
2020-04-18 14:32:09 +0200

    Travel Show View : travel_country display order : start date -> so for visa progress: order by travel_country as well and for the vaccine to be don the order is by id (so asc alpha thx to data in csv)

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,11 +1,11 @@
 class Travel < ApplicationRecord
   include TravelOnboarding
   belongs_to :user
-  has_many :travel_countries, dependent: :destroy
+  has_many :travel_countries, -> { order "start_date ASC" }, dependent: :destroy
   has_many :countries, through: :travel_countries
   has_many :visa_progressions, through: :travel_countries
   has_many :stay_vaccine_progressions, through: :travel_countries
-  has_many :vaccine_progressions, dependent: :destroy
+  has_many :vaccine_progressions, -> { order "vaccine_id ASC" }, dependent: :destroy
 
   accepts_nested_attributes_for :travel_countries
   accepts_nested_attributes_for :countries
