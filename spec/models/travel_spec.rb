fa6512af9b9fcb1d97f44599a4d67a02f99fec7a
marie <marie.blanchet.pro@gmail.com>
2020-04-23 17:58:53 +0200

    add test order for travels (travel_countries: start_date &&  vaccine_progression: vaccine_id)

--- spec/models/travel_spec.rb
+++ spec/models/travel_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe Travel do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:countries) }
-  it { is_expected.to have_many(:travel_countries) }
+  it { is_expected.to have_many(:travel_countries).order('start_date ASC') }
   it { is_expected.to have_many(:visa_progressions) }
   it { is_expected.to have_many(:stay_vaccine_progressions) }
-  it { is_expected.to have_many(:vaccine_progressions) }
+  it { is_expected.to have_many(:vaccine_progressions).order('vaccine_id ASC') }
   it { is_expected.to accept_nested_attributes_for(:travel_countries) }
   it { is_expected.to accept_nested_attributes_for(:countries) }
   it { is_expected.to validate_presence_of(:name) }
