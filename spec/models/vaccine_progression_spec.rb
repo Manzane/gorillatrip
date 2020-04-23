2ff125bfdef17bca7ab3f2f3e5ce955849ef7816
marie <marie.blanchet.pro@gmail.com>
2020-04-23 15:21:53 +0200

    add test of Vaccine_Progression - uniqueness of vaccine_id and travel_id was tricky - to keep an eye on it

--- spec/models/vaccine_progression_spec.rb
+++ spec/models/vaccine_progression_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 describe VaccineProgression do
+
   it { is_expected.to belong_to(:travel) }
   it { is_expected.to belong_to(:vaccine) }
   it { is_expected.to have_many(:stay_vaccine_progressions) }
   it { is_expected.to have_many(:travel_countries) }
-  # it { is_expected.to validate_uniqueness_of(:vaccine).scoped_to(:travel) }
+
+  describe 'uniqueness of travel and vaccine' do
+
+    subject { VaccineProgression.new(vaccine_id: 1, travel_id: 1) }
+
+    context 'when vaccineprogression is not unique' do
+      before { VaccineProgression.create(vaccine_id: 1, travel_id: 1) }
+      it {expect(subject).to be_invalid}
+    end
+
+    # it { is_expected.to validate_uniqueness_of(:vaccine_id).scoped_to(:travel_id)}
+  end
+
 end
