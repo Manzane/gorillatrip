eea85d3577845dd58e8f8fbd0d866c97d7796457
marie <marie.blanchet.pro@gmail.com>
2020-04-25 16:11:12 +0200

    information updated and complete for factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,7 +7,11 @@ FactoryBot.define do
     username {"test"}
     date_of_birth {21.years.ago}
     gender {0}
+    nationality {"fr"}
+    phone_number {"0302010203"}
+    age {}
     confirmed_at {Date.yesterday}
+
   end
 
   factory :document do
@@ -28,6 +32,10 @@ FactoryBot.define do
     id {1}
     name {"Zion"}
     french_name {"Zion"}
+    alpha2code {"ZI"}
+    alpha3code {"ZIN"}
+    region {"Space"}
+    flag_url {"www.flag.com"}
     association :continent
   end
 
@@ -35,6 +43,9 @@ FactoryBot.define do
     id {1}
     name {"Travel au centre de la terre"}
     association :user
+    travel_start_date {}
+    travel_end_date {}
+    onboarding_completed_at {}
   end
 
   factory :travel_country do
@@ -43,11 +54,23 @@ FactoryBot.define do
     association :country
     start_date {Date.yesterday}
     end_date {Date.tomorrow}
+    duration {}
   end
 
   factory :visa do
     id {1}
     association :country
+    name {"Visa"}
+    extra_info {"extra_info"}
+    deliverance_max_date {Date.yesterday}
+    duration {"duration"}
+    price {"price"}
+    embassy {"www.embassy"}
+    application {"www.application"}
+    age_max {30}
+    duration_int {60}
+    validity {"validity"}
+    fr_diplomacy {"www.fr_diplomacy"}
   end
 
   factory :visa_progression do
@@ -60,6 +83,17 @@ FactoryBot.define do
   factory :vaccine do
     id {1}
     name {"HA"}
+    contraindications {"contraindications"}
+    price {10}
+    disease_description {"disease_description"}
+    risks {"risks"}
+    treatment_type {"treatment_type"}
+    treatment {"treatment"}
+    vaccine_schema {"vaccine_schema"}
+    treatment_start {Date.yesterday}
+    treatment_end {Date.tomorrow}
+    injection_max {Date.yesterday}
+    travel_advised {false}
   end
 
   factory :vaccine_country do
@@ -67,6 +101,7 @@ FactoryBot.define do
     association :vaccine
     association :country
     systematic {false}
+    extra_info {}
   end
 
   factory :vaccine_progression do
